9311a9c5e8b203290d2d3e6940594a16f656ebc3
Nikhill Rao <rnikhill@umich.edu>
2017-03-26 13:49:10 -0400

    comments added

--- image-processing/test.py
+++ image-processing/test.py
@@ -23,12 +23,15 @@ COLORS = {
     'orange': (255, 165, 0)
 }
 
+
+# gets the Euclidian distance between two colors
 def get_distance(color1, color2):
     dsq = 0
     for i in range(3):
         dsq += np.power((color1[i] - color2[i]), 2)
     return np.sqrt(dsq)
 
+# gets the closest color in the COLORS dictionary to a pixel in the image
 def get_nearest_color(pixel):
     min_color = 'red'
     min_color_dist = float('inf')
@@ -40,7 +43,7 @@ def get_nearest_color(pixel):
             min_color = color
     return COLORS[min_color]
 
-
+# Gets the size of the output image, 500px max in either direction
 def get_output_size(image):
     height = image.shape[0]
     width = image.shape[1]
@@ -50,7 +53,7 @@ def get_output_size(image):
     scale_factor = MAX_PIXELS / float(max_dim)
     return int(round(width * scale_factor)), int(round(height* scale_factor))
 
-
+# Not in use
 def get_average_color(image, i, j):
     neighbors = 0
     res = [0.0, 0.0, 0.0]
@@ -65,24 +68,29 @@ def get_average_color(image, i, j):
     return (int(round(res[0] / neighbors)), int(round(res[1] / neighbors)), int(round(res[2] / neighbors)))
 
 
-
+# Change each pixel in the image to the nearest color in COLORS based on euclidian distance
 def reduce_colors(image):
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             image[i, j] = get_nearest_color(image[i, j])
 
+# change white pixels to black in the image
 def white_2_black(image):
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             if np.array_equal(image[i, j], [255, 255, 255]):
                 image[i, j] = [0, 0, 0]
 
+# Get an all-black single-channel image with the same dimensions as input
 def get_black_image(image):
     return np.zeros((image.shape[0], image.shape[1]), np.uint8)
 
+# Get an all-black 3-channel image with same dimensions as input
 def get_black_color_image(image):
     return np.zeros(image.shape, np.uint8)
 
+# Get a single-channel  image with the same dimensions as input, where all colors that are the input color 
+# are white, else black
 def isolate_color(image, color):
     res = get_black_image(image)
     color_pixel = COLORS[color]
@@ -92,6 +100,7 @@ def isolate_color(image, color):
                 res[i, j] = 255
     return res
 
+# Get a list of white points in an image. Used to isolate each connected connected components
 def get_white_points(image):
     ccs = cv2.connectedComponentsWithStats(image)
     centroids = np.uint32(ccs[3].round())
@@ -140,6 +149,7 @@ def contains(cc_1, cc_2):
                 return True
     return False
 
+# get a dict mapping each color to a list of lists, where each internal list is a connected component color region
 def restore_image(regions, image):
     res = get_black_color_image(image)
     for color in regions:
